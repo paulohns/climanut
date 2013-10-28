1ffb462f7c1ca08633e901d40910255b01757f94
Silvanna Vianna <salyvianna@gmail.com>
2013-10-28 19:31:06 -0200

    Criação da Facade Cidade

--- src/main/java/com/br/climanut/facade/CidadeFacade.java
+++ src/main/java/com/br/climanut/facade/CidadeFacade.java
@@ -1,7 +1,9 @@
 package com.br.climanut.facade;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+
 import com.br.climanut.bean.Cidade;
 import com.br.climanut.dao.CidadeDao;
 import com.br.climanut.utils.ClimanutExceptions;
@@ -19,19 +21,6 @@ public class CidadeFacade implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	CidadeDao cidadeDao = new CidadeDao();
- 
-    public Cidade find(int id) {
-    	
-    	if(id != 0){
-	    	cidadeDao = new CidadeDao();
-	    	cidadeDao.beginTransaction();
-	        Cidade cidade = cidadeDao.find(id);
-	        cidadeDao.closeTransaction();
-	        return cidade;
-    	} else {
-    		return null;
-    	}
-    }
 
 
 	public List<Cidade> findAll() throws ClimanutExceptions{
